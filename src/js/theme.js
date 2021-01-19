fa6c75c7ba53af4aeaeaccd291ae3dec169fcf41
Olha Pylypova <olhasve@gmail.com>
2021-01-19 18:29:02 +0200

    fixHW10

--- src/js/theme.js
+++ src/js/theme.js
@@ -5,7 +5,7 @@ const Theme = {
 };
 
 const refs ={ 
- theme: document.querySelector('.theme-switch'),
+//  theme: document.querySelector('.theme-switch'),
     input: document.querySelector('#theme-switch-toggle'),
     body: document.querySelector('body'),
   
@@ -14,25 +14,29 @@ const refs ={
 refs.input.addEventListener('change', switchTheme)
 function switchTheme(event) { 
     event.preventDefault();
-    if (event.currentTarget.checked) {
-        addDarkThemeToBody()
-    } else { addLightThemeToBody(); } 
-}
-function addDarkThemeToBody() { 
-    refs.body.classList.add('dark-theme')
-    refs.body.classList.remove('light-theme') 
-    // const darkTheme = JSON.stringify(Theme.DARK);
-    // const parsed = JSON.parse(darkTheme);
-    localStorage.setItem('Theme', Theme.DARK);  
-
-};
-function addLightThemeToBody() { 
-refs.body.classList.remove('dark-theme') 
     refs.body.classList.add('light-theme') 
-    // const lightTheme = JSON.stringify(Theme.LIGHT);
-    // const parsedLight = JSON.parse(lightTheme );
-    localStorage.setItem('Theme', Theme.LIGHT); 
-};
+    if (event.currentTarget.checked) {
+        // addDarkThemeToBody()
+        refs.body.classList.replace('light-theme', 'dark-theme');
+        localStorage.setItem('Theme', Theme.DARK);
+    } else { refs.body.classList.replace('dark-theme','light-theme', );    localStorage.setItem('Theme', Theme.LIGHT);  } 
+}
+// function addDarkThemeToBody() { 
+//     // refs.body.classList.add('dark-theme')
+//     // refs.body.classList.remove('light-theme') 
+//     refs.body.classList.replace(light-theme,dark-theme) 
+//     // const darkTheme = JSON.stringify(Theme.DARK);
+//     // const parsed = JSON.parse(darkTheme);
+//     localStorage.setItem('Theme', Theme.DARK);  
+
+// };
+// function addLightThemeToBody() { 
+// refs.body.classList.replace(light-theme, dark-theme) 
+//     // refs.body.classList.add('light-theme') 
+//     // const lightTheme = JSON.stringify(Theme.LIGHT);
+//     // const parsedLight = JSON.parse(lightTheme );
+// //     localStorage.setItem('Theme', Theme.LIGHT); 
+// };
 const saveTheme = localStorage.getItem("Theme");
 if (saveTheme === Theme.DARK) { 
     refs.body.classList.add('dark-theme')
